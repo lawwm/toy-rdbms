4c8b2a79fbc7edcfda926269f22ed7c4765a2a6d
lawwm <underworlddonutmerchant@gmail.com>
2024-08-29 20:47:01 +0800

    Fix lock manager concurrency

--- src/manager/LockManager.h
+++ src/manager/LockManager.h
@@ -8,208 +8,216 @@
 
 #include "../common.h"
 /*
-	If call get on a scan
-	-> get slock.
+  If call get on a scan
+  -> get slock.
 
 
-	if call write
-	-> get xlock on current block
-	-> get xlock on the next block that is written to...
+  if call write
+  -> get xlock on current block
+  -> get xlock on the next block that is written to...
 
-	Wait-Die
+  Wait-Die
 
-	Ti wants a lock that Tj has
-	Ti has lower priority -> Ti dies
-	Ti has higher priority -> Ti waits
+  Ti wants a lock that Tj has
+  Ti has lower priority -> Ti dies
+  Ti has higher priority -> Ti waits
 
-	Slock can have multiple reader, cannot write
+  Slock can have multiple reader, cannot write
 */
 
 enum LockStatus {
-	SLock,
-	XLock
+  SLock,
+  XLock
 };
 
 struct LockMetaData {
-	LockStatus status;
-	u32 count;
-	std::time_t earliestTime;
+  LockStatus status;
+  u32 count;
+  std::time_t earliestTime;
 };
 
 class LockManager {
 private:
-	std::unordered_map<std::string, std::unordered_map<u64, LockMetaData>> lockMap;
-	std::mutex mut;
-	std::condition_variable cv;
-	u32 defaultWaitTime;
+  std::unordered_map<std::string, std::unordered_map<u64, LockMetaData>> lockMap;
+  std::mutex mut;
+  std::condition_variable cv;
+  u32 defaultWaitTime;
 public:
 
-	LockManager(u32 wt = 5) : lockMap{}, mut{}, cv{}, defaultWaitTime{ wt } {}
+  LockManager(u32 wt = 5) : lockMap{}, mut{}, cv{}, defaultWaitTime{ wt } {}
 
-	// release shared lock
-	bool releaseSLock(PageId& pageId) {
-		{
-			std::lock_guard<std::mutex> lk(mut);
+  // release shared lock
+  bool releaseSLock(PageId& pageId) {
+    {
+      std::lock_guard<std::mutex> lk(mut);
 
-			auto& pageMap = this->lockMap.at(pageId.filename);
-			LockMetaData metadata = pageMap.at(pageId.pageNumber);
-			metadata.count -= 1;
-			if (metadata.count == 0) {
-				pageMap.erase(pageId.pageNumber);
-				if (pageMap.size() == 0) {
-					this->lockMap.erase(pageId.filename);
-				}
-			}
-			else {
-				pageMap[pageId.pageNumber] = metadata;
-			}
-		}
-		cv.notify_all();
-		return true;
-	}
+      auto& pageMap = this->lockMap.at(pageId.filename);
+      LockMetaData metadata = pageMap.at(pageId.pageNumber);
+      metadata.count -= 1;
+      if (metadata.count == 0) {
+        pageMap.erase(pageId.pageNumber);
+        if (pageMap.size() == 0) {
+          this->lockMap.erase(pageId.filename);
+        }
+      }
+      else {
+        pageMap[pageId.pageNumber] = metadata;
+      }
+    }
+    cv.notify_all();
+    return true;
+  }
 
-	bool getSLock(PageId& pageId, std::time_t& txnTime) {
-		return getSLock(pageId, txnTime, defaultWaitTime);
-	}
+  bool getSLock(PageId& pageId, std::time_t& txnTime) {
+    return getSLock(pageId, txnTime, defaultWaitTime);
+  }
 
-	// return false if it should die...
-	bool getSLock(PageId& pageId, std::time_t& txnTime, u32 waitingTime) {
-		bool shouldDie = false;
-		std::time_t startTime = std::time(nullptr);  // Get current time as time_t
+  // return false if it should die...
+  bool getSLock(PageId& pageId, std::time_t& txnTime, u32 waitingTime) {
+    bool shouldDie = false;
+    std::time_t startTime = std::time(nullptr);  // Get current time as time_t
 
-		std::unique_lock<std::mutex> ulock(mut);
-		bool succeeded = cv.wait_for(ulock, std::chrono::seconds(waitingTime), 
-			[this, &shouldDie, &pageId, &txnTime, &startTime, &waitingTime] {
+    std::unique_lock<std::mutex> ulock(mut);
+    bool succeeded = cv.wait_for(ulock, std::chrono::seconds(waitingTime),
+      [this, &shouldDie, &pageId, &txnTime, &startTime, &waitingTime] {
+        // std::cout << "Waiting for S Lock for file " << pageId.filename << " and page " << pageId.pageNumber << std::endl;
+        auto& lockMap = this->lockMap;
+        // if no locks on table
+        if (lockMap.find(pageId.filename) == end(lockMap)) {
+          return true;
+        }
+        auto& pageMap = lockMap.at(pageId.filename);
+        // if no locks on block
+        if (pageMap.find(pageId.pageNumber) == end(pageMap)) {
+          return true;
+        }
 
-			auto& lockMap = this->lockMap;
-			// if no locks on table
-			if (lockMap.find(pageId.filename) == end(lockMap)) {
-				return true;
-			}
-			auto& pageMap = lockMap.at(pageId.filename);
-			// if no locks on block
-			if (pageMap.find(pageId.pageNumber) == end(pageMap)) {
-				return true;
-			}
+        LockMetaData metadata = pageMap.at(pageId.pageNumber);
+        // if is shared lock
+        if (metadata.status == SLock) {
+          return true;
+        }
 
-			LockMetaData metadata = pageMap.at(pageId.pageNumber);
-			// if is shared lock
-			if (metadata.status == SLock) {
-				return true;
-			}
+        std::time_t endTime = std::time(nullptr);  // Do not kill under X seconds
+        double difference = std::difftime(endTime, startTime);
+        if (difference < waitingTime) {
+          return false;
+        }
 
-			std::time_t endTime = std::time(nullptr);  // Do not kill under X seconds
-			double difference = std::difftime(endTime, startTime);
-			if (difference < waitingTime) {
-				return false;
-			}
+        // if current txn has lower priority -> needs to die
+        if (metadata.earliestTime < txnTime) {
+          shouldDie = true;
+          return true;
+        }
 
-			// if current txn has lower priority -> needs to die
-			if (metadata.earliestTime < txnTime) {
-				shouldDie = true;
-				return true;
-			}
+        return false;
+      });
 
-			return false;
-		});
+    if (shouldDie || !succeeded) {
+      ulock.unlock();
+      cv.notify_all();
+      std::this_thread::yield();
+      return false;
+    }
+    else {
+      if (lockMap.find(pageId.filename) == end(lockMap)) {
+        lockMap.insert({ pageId.filename, {} });
+      }
 
-		if (shouldDie || !succeeded) {
-			ulock.unlock();
-			cv.notify_all();
-			return false;
-		}
+      auto& pageMap = lockMap.at(pageId.filename);
+      if (pageMap.find(pageId.pageNumber) == end(pageMap)) {
+        pageMap.insert({ pageId.pageNumber,  LockMetaData{SLock, 1, txnTime} });
+        return true;
+      }
 
-		if (lockMap.find(pageId.filename) == end(lockMap)) {
-			lockMap.insert({ pageId.filename, {} });
-		}
+      LockMetaData metadata = pageMap.at(pageId.pageNumber);
+      if (metadata.earliestTime > txnTime) {
+        pageMap[pageId.pageNumber] = LockMetaData{ SLock, metadata.count + 1, txnTime };
+      }
 
-		auto& pageMap = lockMap.at(pageId.filename);
-		if (pageMap.find(pageId.pageNumber) == end(pageMap)) {
-			pageMap.insert({pageId.pageNumber,  LockMetaData{SLock, 1, txnTime} });
-			return true;
-		}
+      return true;
+    }
+  }
 
-		LockMetaData metadata = pageMap.at(pageId.pageNumber);
-		if (metadata.earliestTime > txnTime) {
-			pageMap[pageId.pageNumber] = LockMetaData{SLock, metadata.count + 1, txnTime};
-		}
+  // release Xclusive lock
+  bool releaseXLock(PageId& pageId) {
+    {
+      std::lock_guard<std::mutex> lk(mut);
 
-		return true;
-	}
+      auto& pageMap = this->lockMap.at(pageId.filename);
+      LockMetaData metadata = pageMap.at(pageId.pageNumber);
+      pageMap.erase(pageId.pageNumber);
+      if (pageMap.size() == 0) {
+        this->lockMap.erase(pageId.filename);
+      }
+    }
+    cv.notify_all();
+    return true;
+  }
 
-	// release Xclusive lock
-	bool releaseXLock(PageId& pageId) {
-		{
-			std::lock_guard<std::mutex> lk(mut);
+  bool getXLock(PageId& pageId, std::time_t& txnTime) {
+    return getXLock(pageId, txnTime, defaultWaitTime);
+  }
 
-			auto& pageMap = this->lockMap.at(pageId.filename);
-			LockMetaData metadata = pageMap.at(pageId.pageNumber);
-			pageMap.erase(pageId.pageNumber);
-			if (pageMap.size() == 0) {
-				this->lockMap.erase(pageId.filename);
-			}
-		}
-		cv.notify_all();
-		return true;
-	}
-	
-	bool getXLock(PageId& pageId, std::time_t& txnTime) {
-		return getXLock(pageId, txnTime, defaultWaitTime);
-	}
+  bool getXLock(PageId& pageId, std::time_t& txnTime, u32 waitingTime) {
+    bool shouldDie = false;
+    std::time_t startTime = std::time(nullptr);  // Get current time as time_t
 
-	bool getXLock(PageId& pageId, std::time_t& txnTime, u32 waitingTime) {
-		bool shouldDie = false;
-		std::time_t startTime = std::time(nullptr);  // Get current time as time_t
+    std::unique_lock<std::mutex> ulock(mut);
+    bool succeeded = cv.wait_for(ulock, std::chrono::seconds(waitingTime),
+      [this, &shouldDie, &pageId, &txnTime, &startTime, &waitingTime] {
+        //std::cout << "Waiting for X Lock for file " << pageId.filename << " and page " << pageId.pageNumber << std::endl;
+        // if no locks on table
+        if (lockMap.find(pageId.filename) == end(lockMap)) {
+          return true;
+        }
 
-		std::unique_lock<std::mutex> ulock(mut);
-		bool succeeded = cv.wait_for(ulock, std::chrono::seconds(waitingTime), 
-			[this, &shouldDie, &pageId, &txnTime, &startTime, &waitingTime] {
-			// if no locks on table
-			if (lockMap.find(pageId.filename) == end(lockMap)) {
-				return true;
-			}
-			
-			// if no locks on block
-			auto& pageMap = lockMap.at(pageId.filename);
-			if (pageMap.find(pageId.pageNumber) == end(pageMap)) {
-				return true;
-			}
+        // if no locks on block
+        auto& pageMap = lockMap.at(pageId.filename);
+        if (pageMap.find(pageId.pageNumber) == end(pageMap)) {
+          return true;
+        }
 
-			std::time_t endTime = std::time(nullptr);  // Do not kill under X seconds
-			double difference = std::difftime(endTime, startTime);
-			if (difference < waitingTime) {
-				return false;
-			}
+        std::time_t endTime = std::time(nullptr);  // Do not kill under X seconds
+        double difference = std::difftime(endTime, startTime);
+        if (difference < waitingTime) {
+          return false;
+        }
 
-			// if current txn has lower priority -> needs to die
-			LockMetaData metadata = pageMap.at(pageId.pageNumber);
-			if (metadata.earliestTime < txnTime) {
-				std::cout << "Dying due to priority at " << txnTime << std::endl;
-				shouldDie = true;
-				return true;
-			}
+        // if current txn has lower priority -> needs to die
+        LockMetaData metadata = pageMap.at(pageId.pageNumber);
+        if (metadata.earliestTime < txnTime) {
+          //std::cout << "Dying due to priority at " << txnTime << std::endl;
+          shouldDie = true;
+          return true;
+        }
 
-			return false;
-		});
+        return false;
+      });
 
-		if (shouldDie || !succeeded) {
-			ulock.unlock();
-			cv.notify_all();
-			return false;
-		}
+    if (shouldDie || !succeeded) {
+      ulock.unlock();
+      cv.notify_all();
+      //std::cout << "notify all" << std::endl;
+      std::this_thread::yield();
+      return false;
+    }
+    else {
+      if (lockMap.find(pageId.filename) == end(lockMap)) {
+        lockMap.insert({ pageId.filename, {} });
+      }
 
-		if (lockMap.find(pageId.filename) == end(lockMap)) {
-			lockMap.insert({ pageId.filename, {} });
-		}
+      auto& pageMap = lockMap.at(pageId.filename);
+      if (pageMap.find(pageId.pageNumber) == end(pageMap)) {
+        pageMap.insert({ pageId.pageNumber,  LockMetaData{XLock, 1, txnTime} });
+      }
+      else {
+        throw std::runtime_error("Not supposed to try to get xlock when block is occupied, programmer error!!!");
+      }
 
-		auto& pageMap = lockMap.at(pageId.filename);
-		if (pageMap.find(pageId.pageNumber) == end(pageMap)) {
-			pageMap.insert({ pageId.pageNumber,  LockMetaData{XLock, 1, txnTime} });
-		}
-		else {
-			throw std::runtime_error("Not supposed to try to get xlock when block is occupied, programmer error!!!");
-		}
+      return true;
+    }
 
-		return true;
-	}
+
+  }
 };
--- tests/test_lockmanager.cpp
+++ tests/test_lockmanager.cpp
@@ -13,140 +13,161 @@ u32 aVar = 0, bVar = 0;
 std::time_t timeA = 10, timeB = 20, timeC = 30, timeD = 40, timeE = 50;
 
 class MockTransaction {
-	std::vector<PageId> pageIdVector;
-	std::vector<bool> isSLock;
-	std::shared_ptr<LockManager> lm;
-	std::time_t txnTime;
-	u32 secs;
+  std::vector<PageId> pageIdVector;
+  std::vector<bool> isSLock;
+  std::shared_ptr<LockManager> lm;
+  std::string txnName;
+  std::time_t txnTime;
+  u32 secs;
 public:
-	MockTransaction(std::shared_ptr<LockManager> lm, std::time_t txtime, u32 seconds) 
-		: lm{ lm }, txnTime{ txtime }, secs{ seconds } {}
+  MockTransaction(std::string txnName, std::shared_ptr<LockManager> lm, std::time_t txtime, u32 seconds)
+    : txnName{ txnName }, lm{ lm }, txnTime{ txtime }, secs{ seconds } {}
 
-	bool getSLock(PageId& pageId) {
-		if (lm->getSLock(pageId, txnTime, secs)) {
-			pageIdVector.push_back(pageId);
-			isSLock.push_back(true);
-			return true;
-		}
-		return false;
-	}
-	bool getXLock(PageId& pageId) {
-		if (lm->getXLock(pageId, txnTime, secs)) {
-			pageIdVector.push_back(pageId);
-			isSLock.push_back(false);
-			return true;
-		}
-		return false;
-	}
+  bool getSLock(PageId& pageId) {
+    getLock(pageId, false, true);
+    if (lm->getSLock(pageId, txnTime, secs)) {
+      pageIdVector.push_back(pageId);
+      isSLock.push_back(true);
+      return true;
+    }
+    getLock(pageId, true, true);
+    return false;
+  }
 
-	~MockTransaction() {
-		std::cout << "Restarting Thread" << std::endl;
-		for (int i = 0; i < isSLock.size(); ++i) {
-			if (isSLock[i]) {
-				lm->releaseSLock(pageIdVector[i]);
-			}
-			else {
-				lm->releaseXLock(pageIdVector[i]);
-			}
-		}
-	}
+  void getLock(PageId& pageId, bool isFailed, bool isSLock) {
+    std::string lock = "X Lock";
+    std::string status = "trying";
+    if (isSLock) {
+      lock = "S Lock";
+    }
+    if (isFailed) {
+      status = "failed";
+    }
+
+    std::cout << "Transaction " << txnName <<
+      " " << status << " to get " << lock << " for file name " << pageId.filename
+      << " and page number " << pageId.pageNumber << std::endl;
+  }
+
+  bool getXLock(PageId& pageId) {
+    getLock(pageId, false, false);
+    if (lm->getXLock(pageId, txnTime, secs)) {
+      pageIdVector.push_back(pageId);
+      isSLock.push_back(false);
+      return true;
+    }
+    getLock(pageId, true, false);
+    return false;
+  }
+
+  ~MockTransaction() {
+    for (int i = 0; i < isSLock.size(); ++i) {
+      if (isSLock[i]) {
+        lm->releaseSLock(pageIdVector[i]);
+      }
+      else {
+        lm->releaseXLock(pageIdVector[i]);
+      }
+    }
+  }
 };
 
 TEST_CASE("Multiple Slock should work") {
-	LockManager lm{1};
+  LockManager lm{ 1 };
 
-	// multiple calls to get slock should work.
-	REQUIRE(lm.getSLock(blockA, timeA) == true);
-	REQUIRE(lm.getSLock(blockA, timeB) == true);
-	REQUIRE(lm.getSLock(blockA, timeC) == true);
+  // multiple calls to get slock should work.
+  REQUIRE(lm.getSLock(blockA, timeA) == true);
+  REQUIRE(lm.getSLock(blockA, timeB) == true);
+  REQUIRE(lm.getSLock(blockA, timeC) == true);
 }
 
 TEST_CASE("Xlock after an SLock should fail if txn time is larger") {
-	LockManager lm{1};
+  LockManager lm{ 1 };
 
-	REQUIRE(lm.getSLock(blockA, timeA) == true);
-	REQUIRE(lm.getXLock(blockA, timeB) == false);
+  REQUIRE(lm.getSLock(blockA, timeA) == true);
+  REQUIRE(lm.getXLock(blockA, timeB) == false);
 }
 
 TEST_CASE("Slock after an XLock should fail if txn time is larger") {
-	LockManager lm{1};
+  LockManager lm{ 1 };
 
-	REQUIRE(lm.getXLock(blockA, timeA) == true);
-	REQUIRE(lm.getSLock(blockA, timeB) == false);
+  REQUIRE(lm.getXLock(blockA, timeA) == true);
+  REQUIRE(lm.getSLock(blockA, timeB) == false);
 }
 
 TEST_CASE("Xlock after an XLock should fail if txn time is larger") {
-	LockManager lm{1};
+  LockManager lm{ 1 };
 
-	REQUIRE(lm.getXLock(blockA, timeB) == true);
-	REQUIRE(lm.getXLock(blockA, timeA) == false);
-	REQUIRE(lm.getXLock(blockA, timeC) == false);
+  REQUIRE(lm.getXLock(blockA, timeB) == true);
+  REQUIRE(lm.getXLock(blockA, timeA) == false);
+  REQUIRE(lm.getXLock(blockA, timeC) == false);
 }
 
 TEST_CASE("Dead Lock should resolve") {
-	std::shared_ptr<LockManager> lm = std::make_shared<LockManager>(1);
+  std::shared_ptr<LockManager> lm = std::make_shared<LockManager>(1);
 
-	// Initialize a barrier for num_threads threads with a completion function
-	std::barrier sync_point(2, []() noexcept {
-		std::cout << "All threads have reached the barrier. Proceeding to the next phase...\n";
-		});
+  // Initialize a barrier for num_threads threads with a completion function
+  std::barrier sync_point(2, []() noexcept {
+    std::cout << "All threads have reached the barrier. Proceeding to the next phase...\n";
+    });
+  std::shared_ptr<std::atomic<int>> counter = std::make_shared<std::atomic<int>>(0);
 
-	std::thread threadA([&lm, &sync_point]() {
-		u32 count = 0;
-		while (true) {
-			MockTransaction txn{ lm, timeA, 1 };
+  std::thread threadA([&lm, &sync_point](std::shared_ptr<std::atomic<int>> counter) {
+    u32 count = 0;
+    while (true) {
+      MockTransaction txn{ "A", lm, timeA, 3 };
 
-			std::cout << "Thread A trying to get X Lock for Block A" << std::endl;
-			if (!txn.getXLock(blockA)) {
-				std::cout << "Thread A can't get X Lock for Block A" << std::endl;
-				continue;
-			}
+      if (!txn.getXLock(blockA)) {
+        std::cout << "Restarting Thread for transaction A " << std::endl;
+        continue;
+      }
 
-			if (count++ == 0) {
-				std::cout << "Wait for sync" << std::endl;
-				sync_point.arrive_and_wait();
-			}
-					
-			std::cout << "Thread A trying to get X Lock for Block B" << std::endl;
-			if (!txn.getXLock(blockB)) {
-				std::cout << "Thread A can't get X Lock for Block B" << std::endl;
-				continue;
-			}
-			
-			// if reach here, end it.
-			break;
-		}
-	});
+      if (count++ == 0) {
+        std::cout << "Wait for sync" << std::endl;
+        sync_point.arrive_and_wait();
+      }
 
-	// FIgure out why 3 restarts for txn
-	// There should only be 1
-	std::thread threadB([&lm, &sync_point]() {
-		u32 count = 0;
-		while (true) {
-			MockTransaction txn{ lm, timeB, 3 };
+      if (!txn.getXLock(blockB)) {
+        std::cout << "Restarting Thread for transaction A " << std::endl;
+        continue;
+      }
 
-			std::cout << "Thread B trying to get X Lock for Block B" << std::endl;
-			if (!txn.getXLock(blockB)) {
-				std::cout << "Thread B can't get X Lock for Block B" << std::endl;
-				continue;
-			}
+      // if reach here, end it.
+      *counter += 1;
+      break;
+    }
+    }, counter);
 
-			if (count++ == 0) {
-				std::cout << "Wait for sync" << std::endl;
-				sync_point.arrive_and_wait();
-			}
+  // FIgure out why 3 restarts for txn
+  // There should only be 1
+  std::thread threadB([&lm, &sync_point](std::shared_ptr<std::atomic<int>> counter) {
+    u32 count = 0;
+    while (true) {
+      MockTransaction txn{ "B", lm, timeB, 1 };
 
-			std::cout << "Thread B trying to get X Lock for Block A" << std::endl;
-			if (!txn.getXLock(blockA)) {
-				std::cout << "Thread B can't get X Lock for Block A" << std::endl;
-				continue;
-			}
+      if (!txn.getXLock(blockB)) {
+        std::cout << "Restarting Thread for transaction B" << std::endl;
+        continue;
+      }
 
-			// if reach here, end it.
-			break;
-		}
-		});
+      if (count++ == 0) {
+        std::cout << "Wait for sync" << std::endl;
+        sync_point.arrive_and_wait();
+      }
 
-	threadA.join();
-	threadB.join();
+      if (!txn.getXLock(blockA)) {
+        std::cout << "Restarting Thread for transaction B" << std::endl;
+        continue;
+      }
+
+      // if reach here, end it.
+      *counter += 1;
+      break;
+    }
+    }, counter);
+
+  threadA.join();
+  threadB.join();
+  REQUIRE(*counter == 2);
+
 }
